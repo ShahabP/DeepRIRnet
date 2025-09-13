cfc988bcac5e08117f6016b43c7fe7b37d8cf1c7
ShahabP <shahab.pasha@outlook.com>
2025-09-13 22:36:47 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,11 @@
+import random
+import numpy as np
+import torch
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
